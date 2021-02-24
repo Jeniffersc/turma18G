b548a7f37a7f8c3c96fd7cd8f43f78f46725cd64
jeniffersc <jene.santos.campos@gmail.com>
2021-02-24 18:10:53 -0300

    Adicionado códigos Java

---
+++ java/Exercicios-prioritarios-24-02/AULAS/src/IntroduçãoAoJAVA/Exercicio3.java
@@ -0,0 +1,28 @@
+package IntroduçãoAoJAVA;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Exercicio3 
+{
+	public static void main (String[] args)
+	{
+		/*
+		 * Faça um sistema que leia o tempo de duração de um evento 
+		 * em uma fábrica expressa em segundos e mostre-o expresso 
+		 * em horas, minutos e segundos. 
+		 */
+		
+		Scanner ler = new Scanner(System.in);
+		
+		int tempoDuracao, horas, min, s;
+
+		System.out.println("Digite o tempo de duração em segundos: ");
+		tempoDuracao = ler.nextInt();
+		
+		horas = tempoDuracao/3600;
+		min = (tempoDuracao % 60) / 60;
+		s = (tempoDuracao % 60) % 60;
+
+		System.out.println("Horas: "+ horas+ " e "+ min+ " e "+ s+ " segundos");
+	}
+}
